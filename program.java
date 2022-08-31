028110b6f49c0d38a6ea359659c7bc4cb80eb58e
utkarsh singh <utkarshsinghrajput16@gmail.com>
2022-08-31 17:11:38 +0530

    .

---
+++ program.java
@@ -0,0 +1,32 @@
+package java;
+import java.util.Scanner;
+
+// public class main
+// {
+//   public static void main(String[] args) {
+//    {
+//     System.out.println("my name is utkarsh");
+//   }
+
+// }
+public class program
+{
+		public static void main(String[] args) 
+	{
+		System.out.println("number daal");
+		int a ;
+		Scanner n =  new Scanner(System.in);
+    a= n.nextInt();
+    // if (a<10) {
+		// 	a-=10;
+		// }
+		// else{
+		// 	a+=10;
+		// }
+		while ( (int) a==22) {
+			System.out.println(" hehehe working");
+			a+=33;
+		}
+		System.out.println("hello its working " + a);
+	}
+}
